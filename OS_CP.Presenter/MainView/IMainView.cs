1ba4453baa3dd8318fd202344e79b38bdd038907
Nikita Grishchenkov <nikita@grishchenkov.ru>
2020-10-10 21:45:28 +0300

    feat(imainview.cs): добавить содержимое интерфейса вида окна Main

--- OS_CP.Presenter/MainView/IMainView.cs
+++ OS_CP.Presenter/MainView/IMainView.cs
@@ -1,9 +1,85 @@
-﻿namespace OS_CP.Presenter
+﻿using System;
+
+namespace OS_CP.Presenter
 {
     /// <summary>
     /// Specific view interface for Main view
     /// </summary>
     public interface IMainView : IView
     {
+        /// <summary>
+        /// Setting function name
+        /// </summary>
+        string FuncName { set; }
+
+        /// <summary>
+        /// Calculating function value table, drawing chart
+        /// </summary>
+        event Action Calculate;
+
+        /// <summary>
+        /// Reading data
+        /// </summary>
+        event Action Open;
+
+        /// <summary>
+        /// Saving data
+        /// </summary>
+        event Action Save;
+
+        /// <summary>
+        /// Exporting data to Excel
+        /// </summary>
+        event Action Export;
+
+        /// <summary>
+        /// Showing about window
+        /// </summary>
+        event Action About;
+
+        /// <summary>
+        /// Showing about window
+        /// </summary>
+        event Action Help;
+
+        /// <summary>
+        /// Showing about window
+        /// </summary>
+        event Action Settings;
+
+        /// <summary>
+        /// Exiting program
+        /// </summary>
+        event Action Exit;
+
+        /// <summary>
+        /// Showing message about error
+        /// </summary>
+        /// <param name="errorMessage"> Error message </param>
+        void ShowError(string errorMessage);
+
+        /// <summary>
+        /// Showing message about warning
+        /// </summary>
+        /// <param name="warningMessage"> Warning message </param>
+        void ShowWarning(string warningMessage);
+
+        /// <summary>
+        /// Showing message about success
+        /// </summary>
+        /// <param name="successMessage"> Success message </param>
+        void ShowSuccess(string successMessage);
+
+        /// <summary>
+        /// Drawing chart in main form
+        /// </summary>
+        /// <param name="table"> Function value table </param>
+        void DrawChart(double[][] table);
+
+        /// <summary>
+        /// Filling table in main form
+        /// </summary>
+        /// <param name="table"> Function value table </param>
+        void DrawTable(double[][] table);
     }
 }
