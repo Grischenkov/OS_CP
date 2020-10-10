72d97b96552a36fbc51e773ddf8aaee0af9bec3d
Nikita Grishchenkov <nikita@grishchenkov.ru>
2020-10-10 23:34:16 +0300

    feat(presenter): добавить интерфес вида и представитель окна Help

---
+++ OS_CP.Presenter/HelpView/HelpPresenter.cs
@@ -0,0 +1,17 @@
+﻿namespace OS_CP.Presenter
+{
+    /// <summary>
+    /// Specific presenter interface for Help view
+    /// </summary>
+    public sealed class HelpPresenter : BasePresenter<IHelpView>
+    {
+        /// <summary>
+        /// Constructor of AboutPresenter class
+        /// </summary>
+        /// <param name="controller"> Application controller </param>
+        /// <param name="view"> View </param>
+        public HelpPresenter(IApplicationController controller, IHelpView view) : base(controller, view)
+        {
+        }
+    }
+}
