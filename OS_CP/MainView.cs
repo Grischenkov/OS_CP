6e600ff5296e92a6735137ad647a5d52067e1af3
Nikita Grishchenkov <nikita@grishchenkov.ru>
2020-10-10 18:23:40 +0300

    feat(program.cs): добавить регистрацию окна Main

--- OS_CP/MainView.cs
+++ OS_CP/MainView.cs
@@ -7,13 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using OS_CP.Presenter;
 
 namespace OS_CP
 {
     /// <summary>
     /// Class of program for work with main screen
     /// </summary>s
-    public partial class MainView : Form
+    public partial class MainView : Form, IMainView
     {
         /// <summary>
         /// Base constructor
