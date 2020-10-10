af06d5000b608ca1faceaec774ec30b073cc65f1
Nikita Grishchenkov <nikita@grishchenkov.ru>
2020-10-10 22:31:53 +0300

    feat(mainpresenter.cs): добавить содержимое представителя окна Main

--- OS_CP.Presenter/MainView/MainPresenter.cs
+++ OS_CP.Presenter/MainView/MainPresenter.cs
@@ -5,6 +5,8 @@
     /// </summary>
     public sealed class MainPresenter : BasePresenter<IMainView>
     {
+        private Function _function; //Object of class Function for work with model
+
         /// <summary>
         /// Constructor of MainPresenter class
         /// </summary>
@@ -12,6 +14,73 @@
         /// <param name="view"> View </param>
         public MainPresenter(IApplicationController controller, IMainView view) : base(controller, view)
         {
+            _function = new Function();
+
+            View.Calculate += () => Calculate();
+            View.Open += Open;
+            View.Save += Save;
+            View.Export += Export;
+            View.About += About;
+            View.Help += Help;
+            View.Settings += Settings;
+            View.Exit += Exit;
+        }
+
+        /// <summary>
+        /// Display data
+        /// </summary>
+        private void Calculate()
+        {
+            View.FuncName = GetFunction();
+            View.DrawChart();
+        }
+
+        /// <summary>
+        /// Showing about form
+        /// </summary>
+        private void Open()
+        {
+            Controller.Run<OpenPresenter>();
+        }
+
+        /// <summary>
+        /// Showing about form
+        /// </summary>
+        private void Save()
+        {
+            Controller.Run<SavePresenter>();
+        }
+
+        /// <summary>
+        /// Showing about form
+        /// </summary>
+        private void Export()
+        {
+            Controller.Run<ExportPresenter>();
+        }
+
+        /// <summary>
+        /// Showing about form
+        /// </summary>
+        private void About()
+        {
+            Controller.Run<AboutPresenter>();
+        }
+
+        /// <summary>
+        /// Showing settings form
+        /// </summary>
+        private void Help()
+        {
+            Controller.Run<HelpPresenter>();
+        }
+
+        /// <summary>
+        /// Showing settings form
+        /// </summary>
+        private void Settings()
+        {
+            Controller.Run<SettingsPresenter>();
         }
     }
 }
