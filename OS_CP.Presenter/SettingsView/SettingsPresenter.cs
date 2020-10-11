f3f9b7536ad7121300d685eb71dad96d0e9307e4
Nikita Grishchenkov <nikita@grishchenkov.ru>
2020-10-11 17:11:15 +0300

    feat(presenter): добавить интерфейс вида и представитель окна Settings

---
+++ OS_CP.Presenter/SettingsView/SettingsPresenter.cs
@@ -0,0 +1,17 @@
+﻿namespace OS_CP.Presenter
+{
+    /// <summary>
+    /// Specific presenter interface for Settings view
+    /// </summary>
+    public sealed class SettingsPresenter : BasePresenter<ISettingsView>
+    {
+        /// <summary>
+        /// Constructor of SettingsPresenter class
+        /// </summary>
+        /// <param name="controller"> Application controller </param>
+        /// <param name="view"> View </param>
+        public SettingsPresenter(IApplicationController controller, ISettingsView view) : base(controller, view)
+        {
+        }
+    }
+}
