af6377decbde3835f783c778762d9cadc4802b18
Nikita Grishchenkov <nikita@grishchenkov.ru>
2020-11-11 22:13:52 +0300

    feat(ifunction.cs): добавить интерфейс модели

---
+++ OS_CP.Model/IFunction.cs
@@ -0,0 +1,31 @@
+﻿namespace OS_CP.Model
+{
+    /// <summary>
+    /// Interface for model
+    /// </summary>
+    public interface IFunction
+    {
+        /// <summary>
+        /// Getting function name
+        /// </summary>
+        string Name { get; }
+
+        /// <summary>
+        /// Getting and setting function value table size
+        /// </summary>
+        int Size { get; set; }
+
+        /// <summary>
+        /// Getting function value table
+        /// </summary>
+        double[][] Table { get; }
+
+        /// <summary>
+        /// Getting and setting i,j element of function value table
+        /// </summary>
+        /// <param name="i"> Row index </param>
+        /// <param name="j"> Column index </param>
+        /// <returns></returns>
+        double this[int i, int j] { get; set; }
+    }
+}
