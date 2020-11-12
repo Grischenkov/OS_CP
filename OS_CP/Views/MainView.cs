aef19c96997e6e789b987de0a1e26f667910bf8e
Nikita Grishchenkov <nikita@grishchenkov.ru>
2020-11-12 21:28:16 +0300

    feat(mainview): добавить проверки значений
    Добавить метод проверки пустых ячеек и перевода string в double в MainPresenter.cs.
    
    Добавить игнорирование пустых ячеек в MainView.cs при передаче их из вида в представитель.

--- OS_CP/Views/MainView.cs
+++ OS_CP/Views/MainView.cs
@@ -35,9 +35,10 @@ namespace OS_CP
                 for (int i = 0; i < Table.RowCount - 1; i++) 
                 {
                     table[i] = new string[3];
-                    table[i][0] = Table.Rows[i].Cells[0].Value.ToString();
-                    table[i][1] = Table.Rows[i].Cells[1].Value.ToString();
-                    table[i][2] = Table.Rows[i].Cells[2].Value.ToString();
+
+                    table[i][0] = Table.Rows[i].Cells[0].Value != null ? Table.Rows[i].Cells[0].Value.ToString() : null;
+                    table[i][1] = Table.Rows[i].Cells[1].Value != null ? Table.Rows[i].Cells[1].Value.ToString() : null;
+                    table[i][2] = Table.Rows[i].Cells[2].Value != null ? Table.Rows[i].Cells[2].Value.ToString() : null;
                 }
                 return table;
             }
