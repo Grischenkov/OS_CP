be65e2aa07fa201200f5be672865aee5b9f19287
Nikita Grishchenkov <nikita@grishchenkov.ru>
2020-11-15 22:17:48 +0300

    feat(bump-version.py): добавить обновление версии на экране загрузки

--- bump-version.py
+++ bump-version.py
@@ -2,27 +2,30 @@
 import sys
 import os
 
-dirname     = os.path.dirname(__file__)
-filename    = os.path.join(dirname, 'OS_CP\Views\AboutView.Designer.cs')
-sub         = "this.Version.Text = "
-curdate     = datetime.datetime.today()
-date        = curdate.strftime("%d/%m/%Y")
+dirname         = os.path.dirname(__file__)
+aboutFilename   = os.path.join(dirname, 'OS_CP\Views\AboutView.Designer.cs')
+splashFilename  = os.path.join(dirname, 'OS_CP\Views\SplashView.Designer.cs')
+sub             = "this.Version.Text = "
+curdate         = datetime.datetime.today()
+date            = curdate.strftime("%d/%m/%Y")
+files           = [aboutFilename, splashFilename]
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
-        file = open(filename, "r")
-        lines = file.readlines()
-        file.close()
+        for filename in files:
+            file = open(filename, "r")
+            lines = file.readlines()
+            file.close()
 
-        i = 0
-        for text in lines:
-            if sub in text:
-                break;
-            i = i + 1
-        lines[i] = "            this.Version.Text = " + '"' + "Version: " + sys.argv[1] + '    ' + date + '"' + ";" + '\n'  
+            i = 0
+            for text in lines:
+                if sub in text:
+                    break;
+                i = i + 1
+            lines[i] = "            this.Version.Text = " + '"' + "Version: " + sys.argv[1] + '    ' + date + '"' + ";" + '\n'  
         
-        file = open(filename, "w")
-        file.writelines(lines)
+            file = open(filename, "w")
+            file.writelines(lines)
 
         print("Version number bumped to " + sys.argv[1])
     else:
