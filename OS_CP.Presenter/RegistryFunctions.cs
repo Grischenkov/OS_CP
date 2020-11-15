b4d5626b2e8e661ec66b76403e39a7158e2ff874
Nikita Grishchenkov <nikita@grishchenkov.ru>
2020-11-15 20:37:32 +0300

    feat(registryfunctions.cs): добавить статический класс работы с реестром
    Добавить функции:
    
    Получение ключа
    Получения значения ключа
    Получения списка ключей
    Сохранения нового значения ключа

---
+++ OS_CP.Presenter/RegistryFunctions.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+namespace OS_CP.Presenter
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class RegistryFunctions
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public static RegistryKey CheckRegistry (string path)
+        {
+            return Registry.CurrentUser.OpenSubKey(path, true) ?? 
+                   Registry.CurrentUser.CreateSubKey(path, true);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public static string GetValue(RegistryKey key, string name)
+        {
+            return key.GetValue(name).ToString();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static string[] GetKeys(RegistryKey key)
+        {
+            return key.GetValueNames();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="name"></param>
+        /// <param name="value"></param>
+        public static void SetValue(RegistryKey key, string name, string value)
+        {
+            key.SetValue(name, value);
+        }
+    }
+}
