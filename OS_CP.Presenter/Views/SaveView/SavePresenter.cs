ebc068883bbe38837c8714c516e7e028886fe4ad
Nikita Grishchenkov <nikita@grishchenkov.ru>
2020-11-16 13:47:30 +0300

    feat(presenter): добавить интерфейс вида и представитель окна Save

---
+++ OS_CP.Presenter/Views/SaveView/SavePresenter.cs
@@ -0,0 +1,17 @@
+﻿namespace OS_CP.Presenter
+{
+    /// <summary>
+    /// Specific presenter interface for Splash view
+    /// </summary>
+    public sealed class SavePresenter : BasePresenter<ISaveView>
+    {
+        /// <summary>
+        /// Constructor of SplashPresenter class
+        /// </summary>
+        /// <param name="controller"> Application controller </param>
+        /// <param name="view"> View </param>
+        public SavePresenter(IApplicationController controller, ISaveView view) : base(controller, view)
+        {
+        }
+    }
+}
