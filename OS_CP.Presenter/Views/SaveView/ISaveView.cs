c87f25fab4a953bceff22abe9deee252266bf737
Nikita Grishchenkov <nikita@grishchenkov.ru>
2020-11-16 18:29:22 +0300

    feat(isaveview.cs): добавить содержимое интерфейса окна Save

--- OS_CP.Presenter/Views/SaveView/ISaveView.cs
+++ OS_CP.Presenter/Views/SaveView/ISaveView.cs
@@ -1,9 +1,15 @@
-﻿namespace OS_CP.Presenter
+﻿using System;
+
+namespace OS_CP.Presenter
 {
     /// <summary>
     /// Specific view interface for Splash View
     /// </summary>
     public interface ISaveView : IView
     {
+        bool SaveValueTable { get; set; }
+        bool SaveChartImage { get; set; }
+        bool SaveChartImageEnabled { set; }
+        event Action Save;
     }
 }
