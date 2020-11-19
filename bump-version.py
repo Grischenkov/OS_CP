f593e9e96ddf260c8bcc73d71e59a86f3e957f05
Nikita Grishchenkov <nikita@grishchenkov.ru>
2020-11-19 18:02:36 +0300

    feat(bump-version.py): добавить изменение версии в свойствах проекта

--- bump-version.py
+++ bump-version.py
@@ -5,7 +5,9 @@ import os
 dirname         = os.path.dirname(__file__)
 aboutFilename   = os.path.join(dirname, 'OS_CP\Views\AboutView.Designer.cs')
 splashFilename  = os.path.join(dirname, 'OS_CP\Views\SplashView.Designer.cs')
+proj            = os.path.join(dirname, 'OS_CP\OS_CP.View.csproj')
 sub             = "this.Version.Text = "
+projSub         = "<ApplicationVersion>" 
 curdate         = datetime.datetime.today()
 date            = curdate.strftime("%d/%m/%Y")
 files           = [aboutFilename, splashFilename]
@@ -27,6 +29,19 @@ if __name__ == "__main__":
             file = open(filename, "w")
             file.writelines(lines)
 
+        file = open(proj, "r")
+        lines = file.readlines()
+        file.close()
+
+        i = 0
+        for text in lines:
+            if projSub in text:
+                break;
+            i = i + 1
+        lines[i] = "    <ApplicationVersion>"+ sys.argv[1] +".0.0</ApplicationVersion>" + '\n'  
+        
+        file = open(proj, "w")
+        file.writelines(lines)
         print("Version number bumped to " + sys.argv[1])
     else:
         print("Relaunch script and type version number. ($python bump-version.py 0.0)")
