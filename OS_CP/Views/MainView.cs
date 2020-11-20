d50a5b1278983013bb29d03c24a0bbbdf6adb8a9
Nikita Grishchenkov <nikita@grishchenkov.ru>
2020-11-20 15:56:39 +0300

    feat: удалить неиспользуемые ссылки

--- OS_CP/Views/MainView.cs
+++ OS_CP/Views/MainView.cs
@@ -1,16 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.IO;
-using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Windows.Forms.DataVisualization.Charting;
-using System.Windows.Forms.Integration;
 using WMPLib;
 using OS_CP.Presenter;
 using OS_CP.Properties;
